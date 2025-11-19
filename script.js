f962a305d13c5edebc817a40a4d765ed93d6e484
Luiz Henrique <luiz.henrique31415@gmail.com>
2025-11-19 09:27:56 -0300

    Add function to get computers choice

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 10) + 1;
+
+    if (randomNumber <= 3) {
+        return "rock";
+    } else if (randomNumber <= 6) {
+        return "paper";
+    } else {
+        return "scissors"
+    }
+}
+
+console.log(getComputerChoice());
