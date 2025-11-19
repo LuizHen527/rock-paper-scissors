71d765fd230a4a4e24574bf30c9ecf3c11d888ca
Luiz Henrique <luiz.henrique31415@gmail.com>
2025-11-19 10:10:43 -0300

    Add winner return value

--- script.js
+++ script.js
@@ -16,29 +16,40 @@ function getHumanChoice() {
 }
 
 function playRound(humanChoice, computerChoice) {
+    let winner = "none";
 
     if (humanChoice === "rock" && computerChoice === "scissors") {
         print("You win! Rock beat scissors.");
+        winner = "human";
     } else {
         print("You lose! Paper beat rock");
+        winner = "computer";
     }
 
     if (humanChoice === "paper" && computerChoice === "rock") {
         print("You win! Paper beat rock.");
+        winner = "human";
     } else {
         print("You lose! Scissor beat paper.");
+        winner = "computer";
     }
 
     if (humanChoice === "scissors" && computerChoice === "paper") {
         print("You win! Scissors beat paper.");
+        winner = "human";
     } else {
         print("You lose! Rock beat scissors.");
+        winner = "computer";
     }
 
+    return winner;
 
 }
 
 
 let humanScore = 0;
 let computerScore = 0;
+let humanChoice = getHumanChoice();
+let computerChoice = getComputerChoice();
 
+let winner = playRound(humanChoice, computerChoice);
