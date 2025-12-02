a4d6678df904a456bdccbcc626a4f7b7b18f088e
Luiz Henrique <luiz.henrique31415@gmail.com>
2025-12-02 11:44:05 -0300

    Refactor end game message

--- script.js
+++ script.js
@@ -63,26 +63,57 @@ function playRound(humanChoice, computerChoice) {
 
 }
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
+function playGame(playerChoice) {
+    const roundsLimit = 5;
+    let humanChoice = playerChoice;
+    let computerChoice = getComputerChoice();
+    let resultsDiv = document.querySelector(".results-div");
 
-    for (let i = 0; i < 5; i++) {
-        let humanChoice = getHumanChoice();
-        let computerChoice = getComputerChoice();
+    let winner = playRound(humanChoice, computerChoice);
 
-        let winner = playRound(humanChoice, computerChoice);
-
-        if (winner === "human") {
-            humanScore++;
-        } else {
-            computerScore++;
-        }
+    if (winner === "human") {
+        humanScore++;
+    } else {
+        computerScore++;
     }
 
-    let winnerMessage = humanScore > computerScore ? "You won the game!" : "You lost the game!"
+    currentRound++;
 
-    alert(`${winnerMessage} Your points: ${humanScore} Computer points: ${computerScore}`);
+    if (currentRound === roundsLimit) {
+        // Delete old paragraphs
+        while (resultsDiv.firstChild) {
+            resultsDiv.removeChild(resultsDiv.firstChild);
+        }
+
+        // Create end message
+
+        let para = document.createElement("p");
+
+        let winnerMessage = humanScore > computerScore ? "You won the game!" : "You lost the game!"
+
+        para.textContent = `${winnerMessage} Your points: ${humanScore} Computer points: ${computerScore}`;
+        
+        resultsDiv.appendChild(para);
+
+        // Reset variables
+        humanScore = 0;
+        computerScore = 0;
+        currentRound = 0;
+    } else {
+        // Delete old paragraphs
+        while (resultsDiv.firstChild) {
+            resultsDiv.removeChild(resultsDiv.firstChild);
+        }
+
+        // Create running score message
+
+        let para = document.createElement("p");
+
+        para.textContent = `Your points: ${humanScore} Computer points: ${computerScore}`;
+        
+        resultsDiv.appendChild(para);
+    }
+
 }
 
 
@@ -90,7 +121,12 @@ const rockButton = document.querySelector(".button-rock");
 const paperButton = document.querySelector(".button-paper");
 const scissorsButton = document.querySelector(".button-scissors");
 
-rockButton.addEventListener('click', () => playRound("rock", getComputerChoice()));
-paperButton.addEventListener('click', () => playRound("paper", getComputerChoice()));
-scissorsButton.addEventListener('click', () => playRound("scissors", getComputerChoice()));
+let humanScore = 0;
+let computerScore = 0;
+let currentRound = 0;
 
+rockButton.addEventListener('click', () => playGame("rock"));
+
+paperButton.addEventListener('click', () => playGame("paper"));
+
+scissorsButton.addEventListener('click', () => playGame("scissors"));
